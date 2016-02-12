f1c0ede9a2673be5a5f2a4665d9d1786ac09e3a8
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-12 10:27:17 +0000

    Add contacts page (solve ex.1.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,5 +23,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+  
+  test "should get contacts" do
+    get :contacts
+    assert_response :success
+    assert_select "title", "Contacts | #{@base_title}"
+  end
 
 end
