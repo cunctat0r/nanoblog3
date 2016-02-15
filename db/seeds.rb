2919ab47215936d931d1afcc9b819af03c95aebc
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-15 11:36:18 +0000

    Create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: 'Test User', email: 'test@email.com', password: 'foobar', password_confirmation: 'foobar')
