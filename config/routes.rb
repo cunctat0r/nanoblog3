f376d80fff9c99ee918effc5044cc4d5ffd35cce
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-16 08:25:51 +0000

    Create routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,13 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessioms#new'
+  post 'login' => 'sessioms#create'
+  delete 'logout' => 'sessions#destroy'
   
   resources :users
+  
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
