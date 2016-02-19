f0221a8d374e3977af66aa4b52fcb29a7446cd93
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-19 10:49:20 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,5 +48,15 @@ class User < ActiveRecord::Base
     self.activation_digest = User.digest(activation_token)
   end
   
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
   
 end
