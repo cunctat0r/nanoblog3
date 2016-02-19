5eda7d9a4bad6ee279a66e0067ca58f99173f441
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-19 10:05:13 +0000

    Add fields to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
+  
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
@@ -72,5 +73,6 @@ private
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
+  
   
 end
